7520b4b6da37872f4d261e4840a59c97946e9ea2
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-09-25 17:32:02 +0000

    Remove CNN and reorganize NPR.

--- sites.js
+++ sites.js
@@ -18,24 +18,18 @@
 
 /**@type {Object<string, Redirect>} */
 const sites = {
-	// https://www.cnn.com/
-	// https://www.cnn.com/2021/09/23/us/gabby-petito-brian-laundrie-update-thursday/index.html
-
-	// https://lite.cnn.com/en
-	// https://lite.cnn.com/en/article/h_0eee0fcdf2364824e37ebee553494dd7
-	// "www.cnn.com": "lite.cnn.com",
 	
-	// https://www.npr.org/
-	// https://www.npr.org/1040078971
-	// https://www.npr.org/sections/coronavirus-live-updates/2021/09/23/1040078971/cdc-covid-19-pfizer-boosters-adults-guidance
-	
-	
-	// https://text.npr.org/
-	// https://text.npr.org/1040078971
 	"www.npr.org": (url) => {
 		const base = "https://text.npr.org/"
 		const pathParts = url.pathname.split("/")
-		if (pathParts.length === 1) return new URL(url.pathname, base)
+		// https://www.npr.org/
+		// https://text.npr.org/
+
+		// https://www.npr.org/1040078971
+		// https://text.npr.org/1040078971
+		if (pathParts.length === 2) return new URL(url.pathname, base)
+		// https://www.npr.org/sections/coronavirus-live-updates/2021/09/23/1040078971/cdc-covid-19-pfizer-boosters-adults-guidance
+		// https://text.npr.org/1040078971
 		else return new URL("/" + pathParts[5], base)
 	},
 
